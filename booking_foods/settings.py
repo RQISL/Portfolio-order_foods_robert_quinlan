2ed126d23b2cb9dffce6663053061514e8e5b6f5
RQISL <creativewb50@gmail.com>
2023-07-31 09:06:13 +0000

    add allowed host for Heroku

--- booking_foods/settings.py
+++ booking_foods/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rqisl-portfolio-order-fo-suy88uwjuf.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-rqisl-portfolio-order-fo-suy88uwjuf.us2.codeanyapp.com',
+                 'oder-foods-and-delivery-a938383e9253.herokuapp.com']
 
 
 # Application definition
